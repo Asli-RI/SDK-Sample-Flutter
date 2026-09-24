1a79bc4d05bdba98d9e4f9267af0869aa7b8a274
__putraAsliRI__ <putra.rolli@asliri.id>
2026-09-24 17:03:54 +0700

    update ios passive liveness

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -5,7 +5,6 @@ import AsliPassiveLiveness
 @main
 @objc class AppDelegate: FlutterAppDelegate, LivenessViewControllerDelegate {
     
-    var windows: UIWindow?
     var livenessResult: String? // Menyimpan hasil liveness
     var flutterResult: FlutterResult? // Menyimpan referensi ke FlutterResult untuk dikembalikan nanti
     
@@ -13,63 +12,63 @@ import AsliPassiveLiveness
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        // Ambil FlutterViewController sebagai root view controller
-        let flutterViewController = FlutterViewController()
+        GeneratedPluginRegistrant.register(with: self)
         
-        // Bungkus FlutterViewController dalam UINavigationController
-        let navigationController = UINavigationController(rootViewController: flutterViewController)
-        navigationController.navigationBar.isHidden = true
-        // Tetapkan UINavigationController sebagai rootViewController dari window
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = navigationController
-        window?.makeKeyAndVisible()
+        let result = super.application(application, didFinishLaunchingWithOptions: launchOptions)
+        
+        if let controller = window?.rootViewController as? FlutterViewController {
+            // Membuat FlutterMethodChannel untuk komunikasi
+            let channel = FlutterMethodChannel(
+                name: "com.asliri.demo/liveness",
+                binaryMessenger: controller.binaryMessenger
+            )
 
-        // Membuat FlutterMethodChannel untuk komunikasi
-        let channel = FlutterMethodChannel(
-            name: "com.asliri.demo/liveness",
-            binaryMessenger: flutterViewController.binaryMessenger
-        )
-
-        // Tangani metode dari Flutter
-        channel.setMethodCallHandler { [weak self] (call, result) in
-            switch call.method {
-            case "startLiveness":
-                self?.flutterResult = result // Simpan referensi ke result
-                self?.startLiveness(controller: flutterViewController, result: result)
-            case "getResult":
-                if let livenessResult = self?.livenessResult {
-                    result(livenessResult)
-                } else {
-                    result(FlutterError(code: "NO_RESULT", message: "No liveness result available", details: nil))
+            // Tangani metode dari Flutter
+            channel.setMethodCallHandler { [weak self, weak controller] (call, result) in
+                guard let controller = controller else { return }
+                switch call.method {
+                case "startLiveness":
+                    self?.flutterResult = result // Simpan referensi ke result
+                    self?.startLiveness(controller: controller, result: result)
+                case "getResult":
+                    if let livenessResult = self?.livenessResult {
+                        result(livenessResult)
+                    } else {
+                        result(FlutterError(code: "NO_RESULT", message: "No liveness result available", details: nil))
+                    }
+                default:
+                    result(FlutterMethodNotImplemented)
                 }
-            default:
-                result(FlutterMethodNotImplemented)
             }
         }
 
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+        return result
     }
     
     private func startLiveness(controller: FlutterViewController, result: @escaping FlutterResult) {
-        let livenessVC = LivenessViewController()
-        livenessVC.navigationItem.hidesBackButton = true
-        livenessVC.delegate = self // Tetapkan delegate
-        // Pastikan controller berada dalam UINavigationController
-        guard let navigationController = controller.navigationController else {
-            result(FlutterError(code: "NAVIGATION_ERROR", message: "No UINavigationController found", details: nil))
-            return
+        DispatchQueue.main.async {
+            let livenessVC = LivenessViewController()
+            livenessVC.delegate = self // Tetapkan delegate
+            
+            let nav = UINavigationController(rootViewController: livenessVC)
+            nav.isNavigationBarHidden = true
+            nav.modalPresentationStyle = .fullScreen
+            
+            controller.present(nav, animated: true, completion: nil)
         }
-        navigationController.isNavigationBarHidden = true
-        // Push LivenessViewController ke dalam stack navigasi
-        navigationController.pushViewController(livenessVC, animated: true)
     }
     
     func didCompleteLiveness(result: String) {
         self.livenessResult = result
         // Kirim kembali hasil ke Flutter melalui FlutterResult
         if let flutterResult = self.flutterResult {
-                flutterResult(result)
-                self.flutterResult = nil // Bersihkan setelah dipanggil
+            flutterResult(result)
+            self.flutterResult = nil // Bersihkan setelah dipanggil
+        }
+        
+        DispatchQueue.main.async { [weak self] in
+            self?.window?.rootViewController?.dismiss(animated: true, completion: nil)
         }
     }
 }
+
